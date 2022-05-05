b2badbe210e9f3f401f94a753c90e2e589c7c8ea
01thn <tikhon.novitski@protonmail.com>
2022-05-05 17:16:23 +0300

    Work with filter

--- src/main/java/com/thn/calculator/filter/AuthFilter.java
+++ src/main/java/com/thn/calculator/filter/AuthFilter.java
@@ -1,13 +1,28 @@
 package com.thn.calculator.filter;
 
+import com.thn.calculator.security.JWTManager;
+
 import javax.servlet.*;
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 public class AuthFilter implements Filter {
     @Override
     public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
         HttpServletRequest httpReq = (HttpServletRequest) servletRequest;
-
+        HttpServletResponse httpResp = (HttpServletResponse) servletResponse;
+        Cookie[] cookies = httpReq.getCookies();
+        String token = null;
+        String login = null;
+        for (Cookie cookie : cookies) {
+            if (cookie.getName().equals("token")) token = cookie.getValue();
+            if (cookie.getName().equals("login")) login = cookie.getValue();
+        }
+        if ((token==null && login==null) || !JWTManager.verifyToken(token,login)){
+            httpResp.sendRedirect("sign-in");
+        }
+        filterChain.doFilter(servletRequest, servletResponse);
     }
 }
